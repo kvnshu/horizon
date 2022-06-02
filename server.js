695841104cfde1eb19328f7b6237b9fb103734c8
K3vX21 <54183095+K3vX21@users.noreply.github.com>
2022-06-02 02:09:37 -0500

    fixed api typo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ if(process.env.NODE_ENV !== 'production'){
 }
 
 const WEATHERSKY_API_KEY = process.env.WEATHERSKY_API_KEY
-const VIRTUALCROSSING_API_KEY = process.env.VIRTUALCROSSING_API_KEY
+const VISUALCROSSING_API_KEY = process.env.VISUALCROSSING_API_KEY
 const axios = require('axios')
 const express = require('express')
 const app = express()
@@ -12,12 +12,12 @@ app.use(express.json())
 app.use(express.static('public'))
 
 app.post('/weather', (req, res) => {
-    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${req.body.latitude}%2C%20${req.body.longitude}?unitGroup=us&include=hours%2Cdays&key=${VIRTUALCROSSING_API_KEY}&contentType=json`
-    // const url = `http://api.weatherstack.com/current?access_key=${WEATHERSKY_API_KEY}&query=${req.body.latitude},${req.body.longitude}&units=f`
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${req.body.latitude}%2C%20${req.body.longitude}?unitGroup=us&include=hours%2Cdays&key=${VISUALCROSSING_API_KEY}&contentType=json`
+    const url2 = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?locations=${req.body.latitude}%2C%20${req.body.longitude}&aggregateHours=1&unitGroup=us&forecastDays=7&shortColumnNames=false &contentType=json&key=${VISUALCROSSING_API_KEY}`
     axios({
-        url: url,
+        url: url2,
         responseType: 'json'
-    }).then(data => res.json(data.data.location))
+    }).then(data => res.json(data.data))
 })
 
 app.listen(8080, () => {
