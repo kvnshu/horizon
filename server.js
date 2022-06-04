9d431e2dccab7636a80ea4896b2c3368014301d1
K3vX21 <54183095+K3vX21@users.noreply.github.com>
2022-06-04 03:38:56 -0500

    multiline url, shorten forecast window call

--- server.js
+++ server.js
@@ -12,18 +12,24 @@ app.use(express.json())
 app.use(express.static('public'))
 
 app.post('/weather', (req, res) => {
-    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${req.body.latitude}%2C%20${req.body.longitude}?unitGroup=us&include=hours%2Cdays&key=${VISUALCROSSING_API_KEY}&contentType=json`
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/`
+              + `${req.body.latitude}%2C%20${req.body.longitude}?unitGroup=us`
+              + `&include=hours%2Cdays`
+              + `&key=${VISUALCROSSING_API_KEY}`
+              + `&contentType=json`
+
     
     const url2 = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?locations=`
-                + `${req.body.latitude}%2C%20${req.body.longitude}`
-                + `&aggregateHours=1`
-                + `&unitGroup=us`
-                + `&forecastDays=7`
-                + `&shortColumnNames=false`
-                + `&contentType=json`
-                + `&key=${VISUALCROSSING_API_KEY}`
+               + `${req.body.latitude}%2C%20${req.body.longitude}`
+               + `&includeAstronomy=true`
+               + `&aggregateHours=1`
+               + `&unitGroup=us`
+               + `&forecastDays=2`
+               + `&shortColumnNames=false`
+               + `&contentType=json`
+               + `&key=${VISUALCROSSING_API_KEY}`
     
-                axios({
+    axios({
         url: url2,
         responseType: 'json'
     }).then(data => res.json(data.data))
