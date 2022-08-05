a2ce23c12473c62d18d3f6e4a2096b3546545608
K3vX21 <54183095+K3vX21@users.noreply.github.com>
2022-08-04 21:56:13 -0500

    load model_1

--- public/model.js
+++ public/model.js
@@ -1,4 +1,4 @@
-const MODEL_URL = './model.json';
+const MODEL_URL = './model/model.json';
 const model = await tf.loadLayersModel(MODEL_URL);
 
 async function predictSunset(humidity, cloudCover, visibility){
