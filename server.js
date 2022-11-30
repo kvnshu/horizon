554ffb8937d07e34e01ea14a2b5ede6c387923df
K3vX21 <54183095+K3vX21@users.noreply.github.com>
2022-11-30 00:29:43 -0500

    remove viewengine setting

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ const port = process.env.PORT || 8080
 app.use(express.json())
 app.use(express.static('public'))
 
-app.set('view engine', 'html');
-
 app.get("/", (req, res) => {
     res.render('index');
 })
